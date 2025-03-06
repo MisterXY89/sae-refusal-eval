495ffbd187c9d27ca5b2ce9ff1b07ad15c80b118
Tilman Kerl <tilmankerl@protonmail.com>
2025-03-06 18:19:17 +0100

    clean up

---
+++ src/utils/templates.py
@@ -0,0 +1,16 @@
+
+TEMPLATE = """You are a helpful chatbot.<|endoftext|>{instruction}<|endoftext|>"""
+
+# PYTHIA_TEMPLATE = """USER: {instruction}
+# ASSISTANT:"""
+
+PYTHIA_TEMPLATE = """Question: {instruction}
+Answer:"""
+
+
+LLAMA_BASE_TEMPLATE = """USER: {instruction}
+ASSISTANT:"""
+
+QWEN_CHAT_TEMPLATE = """<|im_start|>user
+{instruction}<|im_end|>
+<|im_start|>assistant"""
